7759409611bfe6c86c27fc386f98c9326d90ecca
Jose1gnacio <joseignacio.alvarez.n@gmail.com>
2023-12-08 19:47:13 -0300

    agregar barco computer

--- src/GameBoard.js
+++ src/GameBoard.js
@@ -1,34 +1,41 @@
 import React, { useEffect, useState } from 'react';
 import './styles.css';
-import Ship from './Ship';
+
 
 const GameBoard = ({ user }) => {
-  const [board, setBoard] = useState([]);
+    const [board, setBoard] = useState([]);
 
-  useEffect(() => {
-    createBoard();
-  }, [user]); 
+    useEffect(() => {
+        createBoard();
+        addShip();
+    }, [user]);
 
-  const createBoard = () => {
-    const newBoard = [];
+    const createBoard = () => {
+        const newBoard = [];
 
-    for (let i = 0; i < 100; i++) {
-      newBoard.push(<div key={i} className="cell"></div>);
+        for (let i = 0; i < 100; i++) {
+            newBoard.push(<div key={i} id={i} className="cell"></div>);
+        }
+
+        setBoard(newBoard);
+    };
+       
+    function addShip() {
+        const computerCell = document.querySelectorAll('#Computer div')
+        console.log(computerCell) 
     }
+    
 
-    setBoard(newBoard);
-  };
-
-  return (
-    <div className="gameBoardContainer">
-      <div className="user">
-        <h2>{user}</h2>
-        <div className="gameBoard" id={user}>
-          {board}
+    return (
+        <div className="gameBoardContainer">
+            <div className="user">
+                <h2>{user}</h2>
+                <div className="gameBoard" id={user}>
+                    {board}
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default GameBoard;
